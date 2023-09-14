45fc2cbf0f8947e98fcf4e09c7141e45149961da
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-09-14 10:21:12 +0200

    Add console.logs

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 console.log('hello world');
 const greeting = 'Hello world';
 console.log('hello world');
-console.log('hello world');
-console.log('hello world');
+function greet() {
+  console.log(greeting);
+}
+
 console.log('hello world');
 console.log('hello world');
