b662acee5c7a42165a1022b95f619fb064878b1f
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-09-14 11:25:14 +0200

    Add commnands

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ console.log('hello world');
 function greet() {
   console.log(greeting);
 }
+
+const animals = [
+  { id: 1, name: 'Lucia', type: 'Dog', age: 4 },
+  { id: 2, name: 'James', type: 'Goat', age: 4 },
+  { id: 3, name: 'Cathia', type: 'Elephant', age: 4 },
+  { id: 4, name: 'Bob', type: 'Tiger', age: 4 },
+  { id: 5, name: 'Messi', type: 'Turtle', age: 4 },
+];
